9049a1fd1c9ef1c55b1acabdb8e503010428fada
Will Schleter <Will Schleter@160.36.35.219>
2016-08-31 15:22:48 -0400

    Add more help to keydrive

--- keydrive.m
+++ keydrive.m
@@ -5,7 +5,7 @@ set(h,'WindowKeyPressFcn',@dostuff)
 set(h,'UserData',rob)
 set(h,'units','normalized','position',[.6,.6,.3,.3]);
 axis off
-text(0.5,3,{'Drive: arrow keys','Stop: space','Tones: 1-8','Snapshot: P','Quit: Q'});
+text(0.5,3,{'Drive: arrow keys','Faster: F','Slower: S','Stop: space','Tones: 1-8','Snapshot: P','Quit: Q'});
 axis([0,0,3,3]);
 % t = timer;
 % t.Ex
